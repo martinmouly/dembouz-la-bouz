c4818b64cf35d7a89ce5ec9ef7c112a05e6f5f4e
Martin Mouly <martin@mouly.name>
2023-11-21 18:48:12 +0100

    Update main-script.py

--- main-script.py
+++ main-script.py
@@ -1,10 +1,6 @@
 import tweepy
 from dotenv import load_dotenv
 import os
-from bing_image_downloader import downloader
-import json
-import random
-import shutil
 
 # load environment variables
 load_dotenv()
@@ -29,32 +25,12 @@ client_v2 = tweepy.Client(
     access_token_secret=access_token_secret
 )
 
-# Open and read all players from db
-json_file = open('db.json', encoding="utf8")
-data = json.load(json_file)
-json_file.close()
-
-player_name = data['footballers'][random.randint(0,len(data['footballers']) - 1)]["name"]
-print("Selected player : " + player_name)
-
-# Download image from Bing
-search_query= player_name + " playing football"
-downloader.download(search_query, limit=1, verbose=False, output_dir='img')
-print("Download done :D")
-
-# Create tweet with player name + image
-try:
-    mediaId = client_v1.media_upload("img/" + search_query + "/Image_1.jpg")
-except:
-    mediaId = client_v1.media_upload("img/" + search_query + "/Image_1.png")
-
+# fetch img from local storage and tweet 
+mediaId = client_v1.media_upload("img/" + "dembele.jpg")
 client_v2.create_tweet(
-    text=player_name,
+    text="Non.",
     media_ids=[mediaId.media_id_string]
 ) 
 print("Tweet done :D")
 
-# Delete image from local storage
-if os.path.isdir("img"):
-    shutil.rmtree("img")
 
